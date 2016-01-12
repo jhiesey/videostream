5853f3b6884a3da9c60e16bebb02d6d47b5f9d10
John Hiesey <john@hiesey.com>
2016-01-12 03:08:18 +0100

    Rewrite
    This is a complete reimplementation to get away from using mp4box.js,
    which has substantial performance and reliability problems.

--- demo.js
+++ demo.js
@@ -1,20 +1,76 @@
 var videostream = require('./videostream');
-var WebTorrent = require('webtorrent');
+var stream = require('stream')
+var MultiStream = require('multistream')
 
-// This demo uses WebTorrent (https://webtorrent.io)
-var client = new WebTorrent();
+// var WebTorrent = require('webtorrent');
 
-// Sintel torrent from webtorrent.io (https://webtorrent.io/torrents/sintel.torrent)
-var infoHash = '6a9759bffd5c0af65319979fb7832189f4f3c35d';
+// // This demo uses WebTorrent (https://webtorrent.io)
+// var client = new WebTorrent();
 
-client.add({
-	infoHash: infoHash,
-	announce: 'wss://tracker.webtorrent.io/'
-}, function (torrent) {
-	// Got torrent metadata!
-	console.log('Torrent info hash:', torrent.infoHash);
-	// Let's say the first file is a mp4 (h264) video...
-	videostream(torrent.files[0], document.querySelector('video'));
-	var v = document.querySelector('video');
-	v.play();
-});
+// // Sintel torrent from webtorrent.io (https://webtorrent.io/torrents/sintel.torrent)
+// var infoHash = '6a9759bffd5c0af65319979fb7832189f4f3c35d';
+
+// client.add({
+// 	infoHash: infoHash,
+// 	announce: 'wss://tracker.webtorrent.io/'
+// }, function (torrent) {
+// 	// Got torrent metadata!
+// 	console.log('Torrent info hash:', torrent.infoHash);
+// 	// Let's say the first file is a mp4 (h264) video...
+// 	videostream(torrent.files[0], document.querySelector('video'));
+// 	var v = document.querySelector('video');
+// 	v.play();
+// });
+
+var REQUEST_SIZE = 2000000 // 2mb
+
+var http = require('http')
+
+var file = function (opts) {
+	opts = opts || {}
+	var start = opts.start || 0
+	var size = 310674005
+	var end = opts.end ? (opts.end + 1) : size
+
+	var req = null
+	var multi = new MultiStream(function (cb) {
+		var reqStart = start
+		var reqEnd = start + REQUEST_SIZE
+		if (end >= 0 && reqEnd > end) {
+			reqEnd = end
+		}
+		start = reqEnd
+		if (reqStart === reqEnd) {
+			req = null
+			return cb(null, null)
+		}
+		var isBig = (reqEnd - reqStart > 100000)
+
+		req = http.get({
+			path: 'sintel-2048-surround.mp4',
+			// path: '/bbb_sunflower_1080p_30fps_normal.mp4',
+			// path: '/fragmented.mp4',
+			headers: {
+				range: 'bytes=' + reqStart + '-' + (reqEnd - 1)
+			},
+			mode: isBig ? 'prefer-streaming' : 'prefer-fast'
+		}, function (stream) {
+			cb(null, stream)
+		})
+	})
+	var destroy = multi.destroy
+	multi.destroy = function () {
+		if (req) {
+			req.destroy()
+		}
+		destroy.call(multi)
+	}
+	return multi
+}
+
+var video = document.querySelector('video')
+video.addEventListener('error', function (err) {
+	console.error(video.error)
+})
+videostream(file, video)
+video.play()
